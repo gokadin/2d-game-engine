992b1931933d59f35c5a64220d4fe40b3f2dc009
Givi Odikadze <guivio_147@hotmail.com>
2017-03-09 00:45:49 -0500

    fixed movement

--- src/reducers/map/MapReducer.cpp
+++ src/reducers/map/MapReducer.cpp
@@ -9,37 +9,25 @@ void MapReducer::process(action_types type)
     switch (type)
     {
         case action_types::CHARACTER_MOVE_UP:
-            if (!state->character.isMovingDown)
-            {
-                state->character.isMovingUp = true;
-            }
+            state->character.isMovingUp = true;
             break;
         case action_types::CHARACTER_STOP_UP:
             state->character.isMovingUp = false;
             break;
         case action_types::CHARACTER_MOVE_DOWN:
-            if (!state->character.isMovingUp)
-            {
-                state->character.isMovingDown = true;
-            }
+            state->character.isMovingDown = true;
             break;
         case action_types::CHARACTER_STOP_DOWN:
             state->character.isMovingDown = false;
             break;
         case action_types::CHARACTER_MOVE_RIGHT:
-            if (!state->character.isMovingLeft)
-            {
-                state->character.isMovingRight = true;
-            }
+            state->character.isMovingRight = true;
             break;
         case action_types::CHARACTER_STOP_RIGHT:
             state->character.isMovingRight = false;
             break;
         case action_types::CHARACTER_MOVE_LEFT:
-            if (!state->character.isMovingRight)
-            {
-                state->character.isMovingLeft = true;
-            }
+            state->character.isMovingLeft = true;
             break;
         case action_types::CHARACTER_STOP_LEFT:
             state->character.isMovingLeft = false;
--- src/updaters/map/MapUpdater.cpp
+++ src/updaters/map/MapUpdater.cpp
@@ -6,22 +6,22 @@ MapUpdater::MapUpdater(GameState *state):
 
 void MapUpdater::update()
 {
-    if (state->character.isMovingUp)
+    if (state->character.isMovingUp && !state->character.isMovingDown)
     {
         state->map.offsetY -= 10;
     }
 
-    if (state->character.isMovingDown)
+    if (state->character.isMovingDown && !state->character.isMovingUp)
     {
         state->map.offsetY += 10;
     }
 
-    if (state->character.isMovingRight)
+    if (state->character.isMovingRight && !state->character.isMovingLeft)
     {
         state->map.offsetX += 10;
     }
 
-    if (state->character.isMovingLeft)
+    if (state->character.isMovingLeft && !state->character.isMovingRight)
     {
         state->map.offsetX -= 10;
     }
